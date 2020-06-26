4d1517c3d572d92d1250944972682da080c75231
Rasmus <rasmus.kronberg@aalto.fi>
2020-06-26 15:21:33 +0300

    Commented the code

--- slice_split.py
+++ slice_split.py
@@ -16,6 +16,8 @@ import argparse
 
 def slice(n,fs):
 
+	# Function for coarse-graining input trajectory by slicing every nth frame
+
 	i = 0
 	with open(file, 'r') as inp:
 		with open('trj_sliced.xyz', 'w') as out:
@@ -36,6 +38,8 @@ def slice(n,fs):
 
 def split(n):
 
+	# Function for splitting input trajectory into multiple subtrajectories
+
 	i = 0
 	with open(file, 'r') as inp:
 		while True:
@@ -51,9 +55,12 @@ def split(n):
 
 def frame_size(file):
 
+	# Function for calculating the number of lines per frame in input trajectory
+
 	count = None
 	with open(file) as f:
 		for line in f:
+			# Count number of lines between the 1st and 2nd occurence of 'time'
 			if 'time' in line:
 				if count is None:
 					count = 1
@@ -66,7 +73,13 @@ def frame_size(file):
 def main():
 
 	print('Initializing...')
+
+	# Get number of lines per frame in given .xyz file (number of atoms + headers..)
+	# File must contain in each frame one regularly recurring identifier to get number of lines!
 	fs = frame_size(file)
+
+	# n is the number of lines in each subtrajectory in case of splitting OR the number of lines
+	# skipped between two consecutive frames in a coarse-grained trajectory
 	n = int(chunk*fs)
 
 	if SPLIT:
@@ -76,15 +89,16 @@ def main():
 	else:
 		print('Please specify either -sp or -sl!')
 		print('Terminating.')
-
 			
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Split or slice MD trajectory')
 	parser.add_argument('-i','--input',required=True,help='Input trajectory (.xyz)')
 	parser.add_argument('-n','--num',required=True,type=int,
 		help='Stepsize in sliced trajectory/Number of frames in split subtrajectory')
-	parser.add_argument('-sp','--split',action='store_true',help='Split trajectory into subtrajectories')
-	parser.add_argument('-sl','--slice',action='store_true',help='Coarse-grain (slice) trajectory')
+	parser.add_argument('-sp','--split',action='store_true',
+		help='Split trajectory into subtrajectories')
+	parser.add_argument('-sl','--slice',action='store_true',
+		help='Coarse-grain (slice) trajectory')
 	args = vars(parser.parse_args())
 	file = args['input']
 	chunk = args['num']
