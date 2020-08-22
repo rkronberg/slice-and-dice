0db9e0b7f47c3d1427bd4b16f267c91aaa3abdb6
Rasmus <rasmus.kronberg@aalto.fi>
2020-08-22 14:25:40 +0300

    Improvements

--- slice_split.py
+++ slice_split.py
@@ -1,115 +1,56 @@
-
 """
-Tool for splitting and coarse-graining
-(slicing) .xyz MD trajectories. Possibly
-useful for handling huge trajectory files.
-
-TODO: Implement parallel I/O for even 
-larger files
+Tool for splitting and coarse-graining (slicing) .xyz MD trajectories.
+Possibly useful for handling huge trajectory files.
 
 author: Rasmus Kronberg
 rasmus.kronberg@aalto.fi
+
 """
 
-from itertools import islice
-import argparse
-
-def slice(n,fs,start,stop):
-
-	# Function for coarse-graining input trajectory by slicing every nth frame
-
-	i = 0
-	with open(file, 'r') as inp:
-		with open('trj_sliced.xyz', 'w') as out:
-			while i*n < stop:
-				next_n = list(islice(inp, n))
-				if i*n < start:
-					i += 1
-					continue
-				if not next_n:
-					break
-				it = int(i-start/n+1)
-				print('Processing frame number %i' % it, end='\r')
-				j = 0
-				for line in next_n:
-					if j < fs:
-						out.write('%s' % line)
-						j += 1
-					else:
-						break
-				i += 1
-
-			print('\nDone!')
-
-def split(n,start,stop):
-
-	# Function for splitting input trajectory into multiple subtrajectories
-
-	i = 0
-	with open(file, 'r') as inp:
-		while i*n < stop:
-			next_n = list(islice(inp, n))
-			if i*n < start:
-				i+=1
-				continue
-			if not next_n:
-				break
-			it = int(i-start/n+1)
-			print('Processing chunk number %s' % it, end='\r')
-			with open('trj_%s.xyz' % i, 'w') as out:
-				for line in next_n:
-					out.write('%s' % line)
-			i += 1
-
-		print('\nDone!')
-
-def frame_size(file):
-
-	# Function for calculating the number of lines per frame in input trajectory
-
-	count = None
-	with open(file) as f:
-		for line in f:
-			# Count number of lines between the 1st and 2nd occurence of 'time'
-			if 'time' in line:
-				if count is None:
-					count = 1
-				else:
-					break
-			elif count is not None:
-				count += 1
-	return count
+from argparse import ArgumentParser
+import utils
 
 def main():
 
 	print('Initializing...')
 
-	# Get number of lines per frame in given .xyz file (number of atoms + headers..)
-	# File must contain in each frame one regularly occurring pattern to get number of lines!
-	fs = frame_size(file)
-
-	start = int(first*fs)
-	stop = int(last*fs)
-
-	if chunk is None:
-		n = int(ts*fs)
-		slice(n,fs,start,stop)
-	else:
-		n = int(chunk*fs)
-		split(n,start,stop)
-
-if __name__ == '__main__':
-	parser = argparse.ArgumentParser(description='Split or coarse-grain MD trajectory')
-	parser.add_argument('-i','--input',required=True,help='Input trajectory (.xyz)')
-	group = parser.add_mutually_exclusive_group(required=True)
-	group.add_argument('-n','--num',type=int,help='Number of frames in split subtrajectory')
-	group.add_argument('-ts','--timestep',type=int,help='New stepsize in coarse-grained trajectory')
-	parser.add_argument('-f','--first',default=0,type=int,help='First frame to include')
-	parser.add_argument('-l','--last',default=1e32,type=int,help='Last frame to include')
-	args = vars(parser.parse_args())
 	file = args['input']
-	chunk = args['num']
-	ts = args['timestep']
+	regex = args['regex']
+	fsize = args['fsize']
+	nframes = args['nframes']
+	step = args['step']
 	first = args['first']
 	last = args['last']
+
+	with open(file, 'r') as inp:
+		# If fsize is not specified, tries to get number of lines per frame (number 
+		# of atoms + headers) based on given expression that repeats regularly every 
+		# frame (in .xyz files this may be 'time' or 'frame' etc..)
+		if regex is not None:
+			fsize = utils.frame_size(inp, regex)
+			if fsize is None:
+				print('Regex not found. Check your input or set frame size by hand.')
+				quit()
+
+		# Rewind file to specified start and slice or split trajectory 
+		inp.seek(first*fsize)
+		if step is not None:
+			utils.slice(inp, step, fsize, first, last)
+		else:
+			utils.split(inp, nframes, fsize, first, last)
+
+	print('\nDone!')
+
+if __name__ == '__main__':
+	p = ArgumentParser(description='Split or coarse-grain MD trajectory')
+	p.add_argument('-i', '--input', required=True, help='Input trajectory (.xyz)')
+	p.add_argument('-f', '--first', default=0, type=int, help='First frame to include')
+	p.add_argument('-l', '--last', default=int(1e16), type=int, help='Last frame to include')
+	g1 = p.add_mutually_exclusive_group(required=True)
+	g2 = p.add_mutually_exclusive_group(required=True)
+	g1.add_argument('-n', '--nframes', type=int, help='Number of frames in split subtrajectory')
+	g1.add_argument('-ts', '--step', type=int, help='New stepsize in coarse-grained trajectory')
+	g2.add_argument('-r', '--regex', type=str, help='Regular expression to get frame size')
+	g2.add_argument('-fs', '--fsize', type=int, help='Number of lines per frame')
+	args = vars(p.parse_args())
 	main()
---
+++ utils.py
@@ -0,0 +1,44 @@
+from itertools import islice
+
+def slice(inp, step, fsize, first, last):
+
+	# Coarse-grain input trajectory by slicing every nth frame
+	with open('trj_sliced.xyz', 'w') as out:
+		for i, frame in enumerate(range(first, last, step)):
+			next_n = list(islice(inp, step*fsize))
+			if not next_n:
+				break
+			print('Processing frame number %i' % i, end='\r')
+			for j, line in enumerate(next_n):
+				if j < fsize:
+					out.write('%s' % line)
+				else:
+					break
+
+def split(inp, nframes, fsize, first, last):
+
+	# Split input trajectory into multiple subtrajectories
+	for i, frame in enumerate(range(first, last, nframes)):
+		next_n = list(islice(inp, nframes*fsize))
+		if not next_n:
+			break
+		print('Processing chunk number %s' % i, end='\r')
+		with open('trj_%s.xyz' % i, 'w') as out:
+			for line in next_n:
+				out.write('%s' % line)
+
+def frame_size(inp, regex):
+
+	# Calculate the number of lines per frame in input trajectory
+	count = None
+	for line in inp:
+		# Count number of lines between the 1st and 2nd occurence of regex
+		if regex in line:
+			if count is None:
+				count = 1
+			else:
+				break
+		elif count is not None:
+			count += 1
+
+	return count
