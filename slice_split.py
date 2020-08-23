997b07c8a8c13eb16f43827f030c457b0a4ade32
Rasmus <rasmus.kronberg@aalto.fi>
2020-08-23 22:20:05 +0300

    Restructuring

--- slice_split.py
+++ slice_split.py
@@ -8,12 +8,30 @@ rasmus.kronberg@aalto.fi
 """
 
 from argparse import ArgumentParser
-import utils
+import methods
+
+def parse():
+
+	# Parse command line arguments
+
+	p = ArgumentParser(description='Split or coarse-grain MD trajectory')
+	p.add_argument('-i', '--input', required=True, help='Input trajectory (.xyz)')
+	p.add_argument('-f', '--first', default=0, type=int, help='First frame to include')
+	p.add_argument('-l', '--last', default=int(1e16), type=int, help='Last frame to include')
+	g1 = p.add_mutually_exclusive_group(required=True)
+	g2 = p.add_mutually_exclusive_group(required=True)
+	g1.add_argument('-n', '--nframes', type=int, help='Number of frames in split subtrajectory')
+	g1.add_argument('-ts', '--step', type=int, help='New stepsize in coarse-grained trajectory')
+	g2.add_argument('-r', '--regex', type=str, help='Regular expression to get frame size')
+	g2.add_argument('-fs', '--fsize', type=int, help='Number of lines per frame')
+
+	return vars(p.parse_args())
 
 def main():
 
 	print('Initializing...')
 
+	args = parse()
 	file = args['input']
 	regex = args['regex']
 	fsize = args['fsize']
@@ -27,7 +45,7 @@ def main():
 		# of atoms + headers) based on given expression that repeats regularly every 
 		# frame (in .xyz files this may be 'time' or 'frame' etc..)
 		if regex is not None:
-			fsize = utils.frame_size(inp, regex)
+			fsize = methods.frame_size(inp, regex)
 			if fsize is None:
 				print('Regex not found. Check your input or set frame size by hand.')
 				quit()
@@ -35,22 +53,11 @@ def main():
 		# Rewind file to specified start and slice or split trajectory 
 		inp.seek(first*fsize)
 		if step is not None:
-			utils.slice(inp, step, fsize, first, last)
+			methods.slice(inp, step, fsize, first, last)
 		else:
-			utils.split(inp, nframes, fsize, first, last)
+			methods.split(inp, nframes, fsize, first, last)
 
 	print('\nDone!')
 
 if __name__ == '__main__':
-	p = ArgumentParser(description='Split or coarse-grain MD trajectory')
-	p.add_argument('-i', '--input', required=True, help='Input trajectory (.xyz)')
-	p.add_argument('-f', '--first', default=0, type=int, help='First frame to include')
-	p.add_argument('-l', '--last', default=int(1e16), type=int, help='Last frame to include')
-	g1 = p.add_mutually_exclusive_group(required=True)
-	g2 = p.add_mutually_exclusive_group(required=True)
-	g1.add_argument('-n', '--nframes', type=int, help='Number of frames in split subtrajectory')
-	g1.add_argument('-ts', '--step', type=int, help='New stepsize in coarse-grained trajectory')
-	g2.add_argument('-r', '--regex', type=str, help='Regular expression to get frame size')
-	g2.add_argument('-fs', '--fsize', type=int, help='Number of lines per frame')
-	args = vars(p.parse_args())
 	main()
