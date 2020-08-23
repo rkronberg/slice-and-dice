7438f8a67a89a5b612cfe462abacfc521d04f0e8
Rasmus <rasmus.kronberg@aalto.fi>
2020-08-23 22:30:07 +0300

    Updated README.md

--- slice_split.py
+++ slice_split.py
@@ -16,14 +16,14 @@ def parse():
 
 	p = ArgumentParser(description='Split or coarse-grain MD trajectory')
 	p.add_argument('-i', '--input', required=True, help='Input trajectory (.xyz)')
-	p.add_argument('-f', '--first', default=0, type=int, help='First frame to include')
-	p.add_argument('-l', '--last', default=int(1e16), type=int, help='Last frame to include')
 	g1 = p.add_mutually_exclusive_group(required=True)
-	g2 = p.add_mutually_exclusive_group(required=True)
 	g1.add_argument('-n', '--nframes', type=int, help='Number of frames in split subtrajectory')
 	g1.add_argument('-ts', '--step', type=int, help='New stepsize in coarse-grained trajectory')
+	g2 = p.add_mutually_exclusive_group(required=True)
 	g2.add_argument('-r', '--regex', type=str, help='Regular expression to get frame size')
 	g2.add_argument('-fs', '--fsize', type=int, help='Number of lines per frame')
+	p.add_argument('-f', '--first', default=0, type=int, help='First frame to include')
+	p.add_argument('-l', '--last', default=int(1e16), type=int, help='Last frame to include')
 
 	return vars(p.parse_args())
 
